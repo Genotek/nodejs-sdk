277bd4c022b5bae2b3ac18f9b14b9406ce19968c
gusev-genotek <gusev@genotek.ru>
2020-09-02 20:46:12 +0300

    added tag parameter to invoke

--- lib/serverless/functions/v1/invoke.d.ts
+++ lib/serverless/functions/v1/invoke.d.ts
@@ -4,6 +4,6 @@ import { Session } from '../../../../index';
 export class InvokeService {
     constructor(session?: Session);
 
-    invoke(functionId: string, payload?: any): Promise<object>;
+    invoke(functionId: string, payload?: any, tag?: string): Promise<object>;
     wrap(functionId: string): (payload?: any) => Promise<object>;
 }
